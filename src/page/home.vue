a0395eb2c4c338bbd9701aaf861a7a6342b453a4
baptiste geffroy <baptiste@MacBook-Pro-de-baptiste.local>
2022-02-15 10:56:53 +0100

    search with result preview

--- src/page/home.vue
+++ src/page/home.vue
@@ -3,7 +3,19 @@
   <div class='home'>
     <div class="header">
       <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg">
-      <input v-model="user_search_restaurant" type="text" placeholder='De quoi avez vous envie ?'>
+
+      <div class="wrapper--input">
+        <input v-model="user_search_restaurant" type="text" placeholder='De quoi avez vous envie ?'>
+        <div class="search">
+          <div v-for="(restaurant, i) in search_restaurant" :key="i" class="container--restaurant--search">
+            <div class="wrapper--img">
+              <img :src="restaurant.image" alt="">
+            </div>
+            <p>{{restaurant.name}}</p>
+          </div>
+        </div>
+      </div>
+
     </div>
     <div class="bannier"></div>
     <RestaurantRow v-for='(data, i) in data_restaurant' :key='i' :three_restaurant='data' />
@@ -46,7 +58,6 @@ export default {
       for(const restaurant of BDD) {
         const new_restaurant = new Restaurant(restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time)
         all_restaurant.push(new_restaurant)
-
         if(three_restaurant.length === 2) {
           three_restaurant.push(new_restaurant)
           data_restaurant.value.push(three_restaurant)
@@ -59,13 +70,12 @@ export default {
 
     // USER SEARCH RESTAURANT
     let user_search_restaurant = ref('');
+    let search_restaurant = ref([])
 
-    watch(user_search_restaurant, newValue => {
-      let regex = RegExp(newValue)
-      
-      let search_restaurant = all_restaurant.filter(restaurant => regex.test(restaurant.name))
-
-      console.log(search_restaurant);
+    watch(user_search_restaurant, new_value => {
+      let regex = RegExp(new_value.toLowerCase())
+      let new_search_restaurant = all_restaurant.filter(restaurant => regex.test(restaurant.name.toLowerCase()))
+      new_value == 0 ? search_restaurant.value = [] : search_restaurant.value = new_search_restaurant
     })
 
     // LIFECYCLE
@@ -76,6 +86,7 @@ export default {
       data_restaurant, 
       user_search_restaurant,
       all_restaurant,
+      search_restaurant
     }
 
   }
@@ -93,13 +104,46 @@ export default {
     img {
       width: 200px;
     }
-    input {
-      background:#F6F6F6;
-      border: none;
-      width: 400px;
-      height: 60px;
-      outline: none;
-      padding-left: 20px;
+    .wrapper--input{
+      position: relative;
+      input {
+        background:#F6F6F6;
+        border: none;
+        width: 400px;
+        height: 60px;
+        outline: none;
+        padding-left: 20px;
+      }
+      .search {
+        position: absolute;
+        top: 100%;
+        width: 100%;
+        background-color: #fff;
+
+        .container--restaurant--search{
+          display: flex;
+          // justify-content: space-between;
+          align-items: center;
+          padding: 10px;
+
+          &:hover {
+            background-color: #F6F6F6;
+          }
+
+          .wrapper--img {
+            width: 60px;
+            height: 60px;
+            margin-right: 25px;
+            border-radius: 50%;
+            overflow: hidden;
+
+            img {
+              height: 100%;
+              width: auto;
+            }
+          }
+        }
+      }
     }
   }
   .bannier{
